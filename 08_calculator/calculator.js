369470e0f10944184dd94fe9f1a66cd8285cc613
Daniel Kuo <48892016+danielhkuo@users.noreply.github.com>
2024-12-02 20:29:11 -0600

    Completed 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let prod = 1;
+  for (let i = 0; i < arr.length; i++) {
+    prod *= arr[i];
+  }
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(a, pow) {
+	return a**pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 };
 
 // Do not edit below this line
