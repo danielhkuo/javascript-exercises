42266ad939df247f2d4a342c7d5ef350670e7c46
Daniel Kuo <48892016+danielhkuo@users.noreply.github.com>
2024-12-02 20:38:58 -0600

    Completed 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let res = [];
+    for (let i = 0; i < books.length; i++) {
+        res.push(books[i].title)
+    }
+    return res;
 };
 
 // Do not edit below this line
