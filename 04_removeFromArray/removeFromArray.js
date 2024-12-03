5f7e995820bba0358a70ccf2471dbb173e681449
Daniel Kuo <48892016+danielhkuo@users.noreply.github.com>
2024-12-02 20:06:15 -0600

    Completed 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...rest) {
+
+    for (let i = 0; i < rest.length; i++) {
+        let item = rest[i];
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] === item) {
+                arr.splice(j,1);
+                j--;
+            }
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
