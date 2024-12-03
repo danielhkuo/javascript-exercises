e1bf7b2a5abbd92ccf29a461c4e2e19cc34d3754
Daniel Kuo <48892016+danielhkuo@users.noreply.github.com>
2024-12-02 20:35:28 -0600

    Completed 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
-};
+const palindromes = function (input) {
+    input = input.replace(/[^a-z0-9]/gmi, "").replace(/\s+/g, "").toLowerCase();
+    let reverse = input.split('').reverse().join('');
+    return reverse == input;
+};  
 
 // Do not edit below this line
 module.exports = palindromes;
