4974a946b9ebc2162406fa21119103ad7ad0f397
Daniel Kuo <48892016+danielhkuo@users.noreply.github.com>
2024-12-02 20:47:33 -0600

    Completed 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    let currOldestAge = -1;
+    let currOldestIdx = -1;
+    for(let i = 0; i < people.length; i++) {
+        let person = people[i];
+        let age = (person.yearOfDeath == undefined) ? (2024 - person.yearOfBirth):(person.yearOfDeath - person.yearOfBirth);
+        if (age > currOldestAge) {
+            currOldestAge = age;
+            currOldestIdx = i;
+        }
+    }
+    return people[currOldestIdx]
 
 };
 
