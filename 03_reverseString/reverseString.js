0ae86e65002281a471f7d81e70630f924bde59a3
Daniel Kuo <48892016+danielhkuo@users.noreply.github.com>
2024-12-02 19:56:36 -0600

    Completed 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    if (str.length == 0) return '';
+    res = ''
+    for (let i = str.length - 1; i >= 0; i--) {
+        res += str[i];
+    }
+    return res
 };
 
 // Do not edit below this line
