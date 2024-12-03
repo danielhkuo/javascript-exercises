67ef0152b3b9f98003090da41af2dfc7d5cac86c
Daniel Kuo <48892016+danielhkuo@users.noreply.github.com>
2024-12-02 20:14:33 -0600

    Completed 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(l, u) {
 
+    if (!Number.isInteger(l) || !Number.isInteger(u)) {
+        return 'ERROR';
+    }
+
+    if (l < 0 || u < 0) {
+        return 'ERROR';
+    }
+
+    if (l > u) {
+        let temp = u;
+        u = l;
+        l = temp;
+    }
+
+    let sum = 0;
+    for (l; l <= u; l++) {
+        sum += l;
+    }
+    return sum;
 };
 
 // Do not edit below this line
